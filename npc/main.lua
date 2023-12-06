0e18a0d0d77527f241493476247713eff1f255df
_gigabyte <94195465+0x2540be3ff@users.noreply.github.com>
2023-12-06 12:33:29 +0100

    Update main.lua

--- npc/main.lua
+++ npc/main.lua
@@ -1,23 +1,50 @@
-local w = game.Workspace
-local w_table = {
-	
-}
 --[[
-TODO:
-fix the shitty followPath() function
-add customizable tables for easier editing
-add wander() function -> test it
-add waypoints table
-add a stop function so the npc stops moving when in wander mode (incase the shoot function gets called)
-optimize code maybe??
+BETA_VERSION
+Made by xanHR
 
-also if you want it to work you'll need to add this in your acs in the Shoot() function and add a different script in ServerScriptStorage:
-ACS_Framework in ACS_Client
-RS.updateSoundReactor:FireServer(char:FindFirstChild("HumanoidRootPart").Position)
+Added:
+Modifiable tables
+AI Difficulty and NPC Speed
+NPC modes: Wander, Hide and Escape (Run)
+Shooting reaction
 
-add it one line before this in every if statement:
-Evt.Atirar:FireServer(WeaponTool,Suppressor,FlashHider)
+Next update:
+Automatically add needed parts and scripts (manualAdd)
+Sound distance
+Visual reaction (maybe)
+Optimized followPath() function
 ]]
+local w = game.Workspace
+
+
+-------------------------------------------------------------------
+
+
+--local manualAdd = true
+local AIDifficulty = 10 -- from 1 to 20
+local AISpeed = 16 -- walkspeed
+
+
+local w_table = { --wander mode
+	w.ff.Position,
+	w.ff2.Position,
+	w.ff3.Position,
+	w.ff4.Position,
+	w.ff5.Position,
+	w.ff6.Position,
+	w.ff7.Position
+}
+local h_table = { -- hide mode
+	w.hh.Position,
+	w.hh2.Position
+}
+local esc_table = { --escape mode
+	w.esc.Position
+}
+
+
+-------------------------------------------------------------------
+
 
 local PathfindingService = game:GetService("PathfindingService")
 local RunService = game:GetService("RunService")
@@ -25,11 +52,8 @@ local RunService = game:GetService("RunService")
 local path = PathfindingService:CreatePath()
 
 local character = script.Parent
-local AIDifficulty = 10
 local humanoid = character:WaitForChild("Humanoid")
-local part = game.Workspace.Part
-local part2 = game.Workspace.Part2
-local TEST_DESTINATION
+local goto
 local updateSound = game.Workspace.updateSound
 local waypoints -- = {}
 local nextWaypointIndex
@@ -38,31 +62,66 @@ local blockedConnection
 local reach = 2
 local escape = 0
 local hide = 0
-function moveChance()
-	local seedMove = math.random(1,20)
-	print(AIDifficulty, seedMove)
-	if AIDifficulty >= seedMove then
-		TEST_DESTINATION = part2.Position
+local shoot = 0
+--[[
+---DO NOT TOUCH---
+if manualAdd == false then
+	if not w:FindFirstChild("npcWorkspace")	then
+		init_ws()
 	else
-		TEST_DESTINATION = part.Position
+		print("Folder exists!")
+		manualAdd = true
 	end
 end
+
+function init_ws()
+	if manualAdd == false then
+		local folder = Instance.new("Folder")
+		folder.Parent = w
+		folder.Name = "npcWorkspace"
+		local spart = Instance.new("Part")
+		spart.Transparency = 1
+		spart.Position = Vector3.new(0,-1000,0)
+		spart.Size = Vector3.new(4,4,4)
+		spart.CanCollide = false
+		spart.Anchored = true
+		spart.Parent = folder
+		updateSound = spart
+	end
+	
+end
+
+--unused function, will leave it here if I need it
+function moveChance()
+	local seedMove = math.random(1,20)
+	--print(AIDifficulty, seedMove)
+	if AIDifficulty >= seedMove then
+		goto = esc_table[math.random(#esc_table)]
+	else
+		goto = h_table[math.random(#h_table)]
+	end
+end
+]]
 function setRandomPath()
 	local seed = math.random(1,20)
-	print(AIDifficulty, seed)
+	--print(AIDifficulty, seed)
 	if AIDifficulty >= seed then
 		if hide == 0 then
-			TEST_DESTINATION = part2.Position
-			escape = 1
+			if escape == 0 then
+				goto = esc_table[math.random(#esc_table)]
+				escape = 1
+			end
 		end
 	else
 		if escape == 0 then
-			TEST_DESTINATION = part.Position
-			hide = 1
+			if hide == 0 then
+				goto = h_table[math.random(#h_table)]
+				hide = 1
+			end
 		end
 	end
 end
-setRandomPath()
+
 
 local function followPath(destination)
 	local success, errorMessage = pcall(function()
@@ -100,33 +159,44 @@ function getMagnitude(x)
 	local magn = (character.HumanoidRootPart.Position-x).Magnitude
 	return magn
 end
---[[
 function wander()
-	followPath(w_table[math.random(#w_table)])
-	wait(math.random(10,30))
+	if shoot == 0 then
+		wait(math.random(5,10))
+		followPath(w_table[math.random(#w_table)])
+		--print(w_table[math.random(#w_table)])
+	end
 end
-]]
+
 function onTouched(hit)
-	--print(hit.Parent)
 	if not hit or not hit.Parent then return end
 	local human = hit.Parent:findFirstChild("Humanoid")
 	local root = hit.Parent:findFirstChild("HumanoidRootPart")
 	if human and human:IsA("Humanoid") then
-		print(root.Position)
+		--print(root.Position)
 		if getMagnitude(root.Position) < 360 then
+			shoot = 1
 			if reach == 1 then
 				setRandomPath()
 				reach = 0
-				humanoid.WalkSpeed = 24
-				followPath(TEST_DESTINATION)
+				humanoid.WalkSpeed = AISpeed
+				followPath(goto)
 			elseif reach == 2 then
 				setRandomPath()
 				reach = 0
-				humanoid.WalkSpeed = 24
-				followPath(TEST_DESTINATION)
+				humanoid.WalkSpeed = AISpeed
+				followPath(goto)
 			end
 		end
 		tpBack()
 	end
 end
+
 updateSound.Touched:connect(onTouched)
+while task.wait() do
+	if shoot == 1 then
+		--print("Break")
+		break
+	end
+	--print("Task ran")
+	wander()
+end
