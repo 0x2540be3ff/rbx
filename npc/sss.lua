ae98396391931ecdd02acd56ae19843714401047
_gigabyte <94195465+0x2540be3ff@users.noreply.github.com>
2023-12-06 01:25:25 +0100

    Update sss.lua

--- npc/sss.lua
+++ npc/sss.lua
@@ -1,5 +1,6 @@
---/ServerScriptService/--
--- also make sure there is a RemoteEvent called "updateSoundReactor" in the ReplicatedStorage
+--[[ServerScriptService
+also make sure there is a RemoteEvent called "updateSoundReactor" in the ReplicatedStorage
+]]
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local updateSound = game.Workspace.updateSound --just create an invisible part preferably 4,4,4 in size
 local remoteEvent = ReplicatedStorage:FindFirstChild("updateSoundReactor")
@@ -10,5 +11,4 @@ local function update(player, updateSoundPosition)
 end
 
 remoteEvent.OnServerEvent:Connect(update)
---ACS
---RS.updateSoundReactor:FireServer(char:FindFirstChild("HumanoidRootPart").Position)
+
