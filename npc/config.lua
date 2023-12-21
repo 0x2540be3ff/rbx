2b2cbad45b1c3f5451f3a5cfe45c3d3e8a0df211
_gigabyte <94195465+0x2540be3ff@users.noreply.github.com>
2023-12-21 23:19:35 +0100

    Create config.lua

---
+++ npc/config.lua
@@ -0,0 +1,31 @@
+--ModuleScript
+local w = game.Workspace
+local config = {}
+
+--Quick note: The parts in the tables are just a demo, make sure you add your own parts with their own name like this: w.PART_NAME.Position
+
+config.AIDifficulty = 10
+config.AISpeed = 16
+config.fakeDeath = true
+config.visualReaction = true
+config.soundReaction = true
+config.AIWaitTime = {5, 10} --how long the NPC waits until it moves to another part (min, max)
+
+config.w_table = { -- wander_parts
+	w.wander.Position,
+	w.wander1.Position
+}
+config.h_table = { --hide_parts
+	w.hide.Position
+}
+config.esc_table = { --escape_parts
+	w.exit.Position
+}
+config.s_table = { -- sound_distance
+	360,
+	720,
+	1080,
+	1440,
+	1880	
+}
+return config
